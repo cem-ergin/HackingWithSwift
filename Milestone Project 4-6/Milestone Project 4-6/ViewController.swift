9113e1421fc7fe2c5121adf1d1e3b82374dd6382
Cem Ergin <cem-ergin@outlook.com>
2022-06-25 14:15:44 +0100

    Complete milestone project day 32

--- Milestone Project 4-6/Milestone Project 4-6/ViewController.swift
+++ Milestone Project 4-6/Milestone Project 4-6/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    var shoppingList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Shopping List"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItem))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearItems))
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ShopCell", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItem(){
+        let ac = UIAlertController(title: "Add item", message: "Please add your item", preferredStyle: .alert)
+        ac.addTextField()
+        
+        let addAction = UIAlertAction(title: "Submit", style: .default){
+            [weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self.shoppingList.append(item)
+            self.tableView.reloadData()
+        }
+        
+        ac.addAction(addAction)
+        present(ac, animated: true)
+    }
+    
+    @objc func clearItems(){
+        shoppingList.removeAll()
+        tableView.reloadData()
+    }
 }
 
