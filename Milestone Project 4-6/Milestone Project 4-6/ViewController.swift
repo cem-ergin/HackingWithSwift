e5856a5fc0fc5e70395233fceb188be23a102656
Cem Ergin <cem-ergin@outlook.com>
2022-06-25 13:54:35 +0100

    Add milestone project 4-6

---
+++ Milestone Project 4-6/Milestone Project 4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Project 4-6
+//
+//  Created by CTW01856-Admin on 25/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Project 6b - Advanced Layouts/Project 6b - Advanced Layouts/ViewController.swift
+++ Project 6b - Advanced Layouts/Project 6b - Advanced Layouts/ViewController.swift
@@ -61,7 +61,7 @@ class ViewController: UIViewController {
         var previous: UILabel?
         
         for label in [label1, label2, label3, label4, label5] {
-            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
             label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
             label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor,
                                           multiplier: 0.2, constant: -10).isActive = true
