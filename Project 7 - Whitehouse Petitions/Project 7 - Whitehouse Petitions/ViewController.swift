94407cb1b6297b5e7df5881c44200b8bc12553e4
Cem Ergin <cem-ergin@outlook.com>
2022-06-25 22:32:41 +0100

    Complete challenges of day 35, Project 7. Update readme.md

--- Project 7 - Whitehouse Petitions/Project 7 - Whitehouse Petitions/ViewController.swift
+++ Project 7 - Whitehouse Petitions/Project 7 - Whitehouse Petitions/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UITableViewController {
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+    var clearButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         var urlString: String
+        
+        clearButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(clearData))
+        let showInfoButton = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showInfo))
+        let searchButton = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filter))
+        navigationItem.rightBarButtonItems = [showInfoButton, searchButton]
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -31,6 +38,47 @@ class ViewController: UITableViewController {
         
     }
     
+    @objc func clearData (){
+        filteredPetitions = petitions
+        reloadData()
+    }
+    
+    @objc func filter() {
+        let ac = UIAlertController(title: "Filter", message: "Type something to search", preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "OK", style: .default) {
+            [weak ac] _ in
+            if let text = ac?.textFields?[0].text {
+                self.filterData(filterString: text)
+            }
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func filterData (filterString: String) {
+        if(filterString.isEmpty){
+            filteredPetitions = petitions
+        }else{
+            filteredPetitions = petitions.filter({ Petition in
+                return Petition.body.contains(filterString) || Petition.title.contains(filterString)
+            })
+        }
+        reloadData()
+    }
+    
+    func reloadData () {
+        navigationItem.leftBarButtonItem = filteredPetitions.count != petitions.count ? clearButton : nil
+        tableView.reloadData()
+    }
+    
+    
+    @objc func showInfo() {
+        let ac = UIAlertController(title: "Credits", message: "All the data coming from 'We The People Api' of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
     func showError(){
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed. Please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
@@ -42,17 +90,18 @@ class ViewController: UITableViewController {
         
         if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
             petitions = jsonPetitions.results
-            tableView.reloadData()
+            filteredPetitions = petitions
+            reloadData()
         }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return petitions.count
+        return filteredPetitions.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let petition = petitions[indexPath.row]
+        let petition = filteredPetitions[indexPath.row]
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
@@ -60,7 +109,7 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let vc = DetailViewController()
-        vc.detailItem = petitions[indexPath.row]
+        vc.detailItem = filteredPetitions[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
     
--- README.md
+++ README.md
@@ -302,4 +302,4 @@ label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, mu
 
 - [ ] Add a Credits button to the top-right corner using UIBarButtonItem. When this is tapped, show an alert telling users the data comes from the We The People API of the Whitehouse.
 - [ ] Let users filter the petitions they see. This involves creating a second array of filtered items that contains only petitions matching a string the user entered. Use a UIAlertController with a text field to let them enter that string.
-- [ ] Experiment with the HTML – this isn’t a HTML or CSS tutorial, but you can find lots of resources online to give you enough knowledge to tinker with the layout a little.
+- [ ] Experiment with the HTML
