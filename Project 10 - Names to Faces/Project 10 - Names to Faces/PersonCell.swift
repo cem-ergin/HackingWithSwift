a42fc92d01a22f37eb8c0245a1f5376b08fa0cfa
Cem Ergin <cem-ergin@outlook.com>
2022-07-06 21:32:46 +0100

    Create: Project 10 Names to Faces

---
+++ Project 10 - Names to Faces/Project 10 - Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project 10 - Names to Faces
+//
+//  Created by Cem Ergin on 06/07/2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}
