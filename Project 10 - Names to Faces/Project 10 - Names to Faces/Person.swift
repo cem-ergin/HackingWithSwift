7cc653ac88c156de733a65687e839c2b4c17a4d7
Cem Ergin <cem-ergin@outlook.com>
2022-07-07 22:30:47 +0100

    Complete: day 43

---
+++ Project 10 - Names to Faces/Project 10 - Names to Faces/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project 10 - Names to Faces
+//
+//  Created by Cem Ergin on 07/07/2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image:String) {
+        self.name = name
+        self.image = image
+    }
+}
