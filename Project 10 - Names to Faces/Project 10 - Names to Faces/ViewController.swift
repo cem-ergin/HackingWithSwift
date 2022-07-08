ee16e7414303f9d95f464a5185019b45e0021883
Cem Ergin <cem-ergin@outlook.com>
2022-07-08 10:24:55 +0100

    Day 44 Project 10 Challenge 1

--- Project 10 - Names to Faces/Project 10 - Names to Faces/ViewController.swift
+++ Project 10 - Names to Faces/Project 10 - Names to Faces/ViewController.swift
@@ -82,6 +82,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
+        let ac = UIAlertController(title: "Choose action", message: nil, preferredStyle: .alert)
+
+        ac.addAction(UIAlertAction(title: "Rename", style: .default) { _ in
+            self.renamePerson(person: person)
+        })
+        
+        ac.addAction(UIAlertAction(title: "Delete", style: .default) { _ in
+            self.people.remove(at: indexPath.item)
+            self.collectionView.reloadData()
+        })
+        
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        present(ac, animated: true)
+    }
+    
+    func renamePerson(person: Person) {
         let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
         ac.addTextField()
         
