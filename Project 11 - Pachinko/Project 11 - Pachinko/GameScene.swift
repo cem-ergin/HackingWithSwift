44ac65d8314e6d007f9c5e4e3044c1ace95011d4
Cem Ergin <cem-ergin@outlook.com>
2022-07-13 23:48:35 +0100

    Complete: Project 11 challenge three Add: ball count Add: ball count label Add: reload label to reload Add: alert to show its finished or game over

--- Project 11 - Pachinko/Project 11 - Pachinko/GameScene.swift
+++ Project 11 - Pachinko/Project 11 - Pachinko/GameScene.swift
@@ -8,28 +8,22 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene, SKPhysicsContactDelegate {
+class GameScene: SKScene, SKPhysicsContactDelegate, Alertable {
     let balls = ["ballBlue","ballCyan","ballGreen","ballGrey","ballPurple","ballRed","ballYellow"]
     
-    var scoreLabel: SKLabelNode!
-    
-    var score = 0 {
+    var didBallTouchedBlock = false
+        
+    var ballCountLabel: SKLabelNode!
+    var ballCount = 5 {
         didSet {
-            scoreLabel.text = "Score: \(score)"
-        }
-    }
-    
-    var editLabel: SKLabelNode!
-    
-    var editingMode: Bool = false {
-        didSet {
-            if editingMode {
-                editLabel.text = "Done"
-            } else {
-                editLabel.text = "Edit"
+            ballCountLabel.text = "Balls left: \(ballCount)"
+            if(ballCount == 0){
+                showAlert(withTitle: "Game Over", message: "You don't have any ball left :(", completion: reload)
             }
         }
     }
+    
+    var reloadLabel: SKLabelNode!
     
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
@@ -38,16 +32,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         background.zPosition = -1
         addChild(background)
         
-        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
-        scoreLabel.text = "Score: 0"
-        scoreLabel.horizontalAlignmentMode = .right
-        scoreLabel.position = CGPoint(x:980, y:700)
-        addChild(scoreLabel)
+        ballCountLabel = SKLabelNode(fontNamed: "Chalkduster")
+        ballCountLabel.horizontalAlignmentMode = .right
+        ballCountLabel.position = CGPoint(x:980, y:700)
+        addChild(ballCountLabel)
+        ballCount = 5
         
-        editLabel = SKLabelNode(fontNamed: "Chalkduster")
-        editLabel.text = "Edit"
-        editLabel.position = CGPoint(x: 80, y: 700)
-        addChild(editLabel)
+        reloadLabel = SKLabelNode(fontNamed: "Chalkduster")
+        reloadLabel.text = "Reload"
+        reloadLabel.position = CGPoint(x: 80, y: 700)
+        addChild(reloadLabel)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
@@ -62,26 +56,31 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncer(at: CGPoint(x:512, y:0))
         makeBouncer(at: CGPoint(x:768, y:0))
         makeBouncer(at: CGPoint(x:1024, y:0))
+                
+        createBoxes()
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        let objects = nodes(at: location)
-        
-        if objects.contains(editLabel) {
-            editingMode.toggle()
-        } else {
-            if editingMode {
-                let size = CGSize(width: Int.random(in: 16...128), height: 16)
-                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
-                box.zRotation = CGFloat.random(in: 0...3)
-                box.position = location
-                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
-                box.physicsBody?.isDynamic = false
-                addChild(box)
-            } else {
+    fileprivate func createBoxes() {
+        for _ in 0..<50 {
+            createBox(CGPoint(x: CGFloat.random(in: 0...frame.width), y: CGFloat.random(in: (frame.height * 0.4)...(frame.height * 0.7))))
+        }
+    }
+    
+    fileprivate func createBox(_ location: CGPoint) {
+        let size = CGSize(width: Int.random(in: 32...128), height: 16)
+        let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+        box.zRotation = CGFloat.random(in: 0...3)
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+        box.physicsBody?.isDynamic = false
+        box.name = "box"
+        addChild(box)
+    }
+    
+    fileprivate func createBall(_ location: CGPoint) {
+        if ballCount > 0 {
+            if self.childNode(withName: "ball") == nil {
                 let ball = SKSpriteNode(imageNamed: balls.randomElement() ?? "ballRed")
                 ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
                 ball.physicsBody?.restitution = 0.4
@@ -91,6 +90,30 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 addChild(ball)
             }
         }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let objects = nodes(at: location)
+        
+        if objects.contains(reloadLabel) {
+            reload()
+        } else {
+            didBallTouchedBlock = false
+            createBall(location)
+        }
+    }
+    
+    func reload () {
+        for node in self.children {
+            if node.name == "box" || node.name == "ball" {
+                node.removeFromParent()
+            }
+        }
+        
+        self.ballCount = 5
+        self.createBoxes()
     }
     
     func makeBouncer(at position: CGPoint) {
@@ -133,10 +156,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collision(between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
-            score += 1
+            if didBallTouchedBlock {
+                ballCount += 1
+            } else {
+                ballCount -= 1
+            }
         } else if object.name == "bad"{
             destroy(ball: ball)
-            score -= 1
+            ballCount -= 1
         }
     }
     
@@ -149,6 +176,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         ball.removeFromParent()
     }
     
+    
+    func destroy(box: SKNode) {
+        if let sparkParticles = SKEmitterNode(fileNamed: "SparkParticles") {
+            sparkParticles.position = box.position
+            addChild(sparkParticles)
+        }
+        
+        box.removeFromParent()
+    }
+    
     func didBegin(_ contact: SKPhysicsContact) {
         guard let nodeA = contact.bodyA.node else { return }
         guard let nodeB = contact.bodyB.node else { return }
@@ -158,6 +195,34 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         } else if nodeB.name == "ball" {
             collision(between: nodeB, object: nodeA)
         }
+        
+        if nodeA.name == "box" {
+            destroy(box: nodeA)
+            didBallTouchedBlock = true
+        } else if nodeB.name == "box" {
+            destroy(box: nodeB)
+            didBallTouchedBlock = true
+        }
+        
+        if self.childNode(withName: "box") == nil {
+            showAlert(withTitle: "You won!", message: "Wanna play more?", completion: reload)
+        }
     }
     
+}
+
+
+protocol Alertable { }
+extension Alertable where Self: SKScene {
+    func showAlert(withTitle title: String, message: String, completion: @escaping () -> Void) {
+
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+
+        let okAction = UIAlertAction(title: "OK", style: .cancel) { _ in
+            completion()
+        }
+        alertController.addAction(okAction)
+
+        view?.window?.rootViewController?.present(alertController, animated: true)
+    }
 }
