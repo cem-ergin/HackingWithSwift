2b59de11ae7a66963bc91479f6ff75035ca51cbc
Cem Ergin <cem-ergin@outlook.com>
2022-08-07 21:38:36 +0100

    Complete: day 52

---
+++ Project 13 - Instafilter/Project 13 - Instafilter/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Project 13 - Instafilter
+//
+//  Created by Cem Ergin on 07/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var intensity: UISlider!
+    @IBOutlet var imageView: UIImageView!
+    
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    
+}
+
